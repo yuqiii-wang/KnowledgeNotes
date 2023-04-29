9da4a9691f8a646e66690d03aa064f2cc18f6b84
yuqi <yuqiii.wang@qq.com>
2023-04-29 13:34:14 +0800

    some leetcode

---
+++ data_structure_and_algos/algos/recursive/sudoku.cpp
@@ -0,0 +1,6 @@
+// use bit to represent number's presence
+// (011000100)_2 represent the number 3, 7 and 8 have shown up in a line/block
+
+// bit operation (bitset): by complement, there is (~b)+1 = (-b), 
+// where ~b is the bit flipping operation taking 0 -> 1 and 1 -< 0
+
